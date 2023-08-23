7153114be87bcee5d25069be31fd36904363ecf2
duncanolesen <dolesen@caci.co.uk>
2023-08-23 08:51:36 +0100

    Force unrouteable link
    Change the target port to flag link between steps 38 and 12 cannot be routed in order to test dragging to a different target port

--- Examples/ProcessFlow/test/visualization01/common-config.js
+++ Examples/ProcessFlow/test/visualization01/common-config.js
@@ -2,13 +2,13 @@
 // import dataConfig from './data-bp025.json';
 // import dataConfig from './data-bp025-redrawn.json';
 // import dataConfig from './data-bp027.json';
-// import dataConfig from './data-bp100.json';
+import dataConfig from './data-bp100.json';
 // import dataConfig from './data-bp101.json';
 // import dataConfig from './data-bp108.json';
 // import dataConfig from './data-bp124.json';
 // import dataConfig from './data-bp135.json';
 // import dataConfig from './data-bp150.json';
-import dataConfig from './data-bpmn01.json';
+// import dataConfig from './data-bpmn01.json';
 // import dataConfig from './data-bpXYZ.json';
 // import dataConfig from './data-mro1-3 compact.json';
 // import dataConfig from './data-mro1-3.json';
