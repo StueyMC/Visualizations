90d975ecb2c6f1c163644f398e91aca0417bcc93
duncanolesen <dolesen@caci.co.uk>
2024-04-28 09:24:58 +0100

    Attempt to use real MooD Config Data

--- Examples/vNetwork/src/visualization01/src/types/data.d.ts
+++ Examples/vNetwork/src/visualization01/src/types/data.d.ts
@@ -1,16 +1,28 @@
 declare namespace Vis {
   interface Data {
-    ExampleVariable1: string,
-    ExampleVariable2: boolean,
-    ExampleVariable3: Array<Vis.Data.Query>,
+    nodes: Array<Vis.Data.Node>,
+    links: Array<Vis.Data.Link>,
   }
 }
 
 declare namespace Vis.Data {
   
-  interface Query extends MooDElement {
-    key: ID,
-    Name: string,
-    Value: Any,
+  interface Node extends MooDElement{
+    id: ID,
+    name: string,
+    x?: number,
+    y?: number,
+  }
+  
+  interface Link extends MooDElement{
+    id: ID,
+    name: string,
+    source: Vis.Data.EndPoint,
+    target: Vis.Data.EndPoint,
+  }
+  
+  interface EndPoint extends MooDElement{
+    id: ID,
+    name: string,
   }
 }
--- Examples/vNetwork/src/visualization01/visualization.ts
+++ Examples/vNetwork/src/visualization01/visualization.ts
@@ -1,15 +1,18 @@
-import { createApp } from 'vue'
+import { createApp } from "vue"
 import App from "./App.vue"
+import * as vNG from "v-network-graph"
 
 // import Logger from "@helpers/logger";
 // import { ConfigContext } from "@helpers/context/configContext";
-// import {
-//   getVisualizationConfig,
-//   getVisualizationState,
-//   setVisualizationConfig,
-// } from "@helpers/config";
+import {
+  getVisualizationConfig,
+  getVisualizationState,
+  setVisualizationConfig,
+  getVisualizationData,
+} from "@helpers/config";
 import "./visualization.css";
 import { MooDConfig } from "@moodtypes/index";
+// import { Vis } from "@core/src/types/data";
 
 //
 //    Entry function declaration
@@ -19,13 +22,30 @@ export function visualization(config: MooDConfig) {
   // Logger.Log("Development mode is enabled, logging will be enabled");
 
   // Update the global Custom Visualization config object
-  // setVisualizationConfig(config);
+  setVisualizationConfig(config);
 
   // Example ways to reference config variables before we enter React
   const width = parseFloat(config.width);
   const height = parseFloat(config.height);
   const animation = config.animation;
   // const state = getVisualizationState(true);
+
+  // console.log("Type of config: " + typeof config)
+  // const nodes: vNG.Nodes = {}
+  // const nodes = {}
+  var nodes: Record<string,any> = {}
+  const data = getVisualizationData(false)
+  if (data && data.nodes) {
+    for (let node of data.nodes) {
+      // console.log(JSON.stringify(node))
+      const id: string = node.id;
+      nodes[id] = { name: node.name }
+    }
+  }
+  // data.nodes?.forEach((node) => {
+  //   // nodes[node.id] = { name: node.name }
+  //   console.log(JSON.stringify(node))
+  // })
 
   // Logger.Log("This is an example of a development log");
 
