f40e9e90e6156f537973913d81ea14e4906d931a
duncanolesen <dolesen@caci.co.uk>
2024-05-04 18:46:59 +0100

    Create layout using d3-force

--- Examples/vNetwork/src/visualization01/components/Paths.vue
+++ Examples/vNetwork/src/visualization01/components/Paths.vue
@@ -4,7 +4,14 @@ import {
   getVisualizationState,
   getVisualizationData,
 } from "@helpers/config";
+// import { reactive, ref } from "vue"
 import * as vNG from "v-network-graph"
+import {
+  ForceLayout,
+  ForceNodeDatum,
+  ForceEdgeDatum,
+} from "v-network-graph/lib/force-layout"
+
 
 const data = getVisualizationData(false)
 const nodes: vNG.Nodes = {};
@@ -12,6 +19,32 @@ const edges: vNG.Edges = {};
 const layouts: vNG.Layouts = {nodes: {}};
 const paths: vNG.Paths = {};
 const configs = vNG.defineConfigs({
+  view: {
+      layoutHandler: new ForceLayout({
+        positionFixedByDrag: false,
+        positionFixedByClickWithAltKey: true,
+        createSimulation: (d3, nodes, edges) => {
+          // d3-force parameters
+          const forceLink = d3.forceLink<ForceNodeDatum, ForceEdgeDatum>(edges).id(d => d.id)
+          return d3
+            .forceSimulation(nodes)
+            .force("edge", forceLink.distance(40).strength(0.5))
+            .force("charge", d3.forceManyBody().strength(-800))
+            .force("center", d3.forceCenter().strength(0.05))
+            .alphaMin(0.001)
+
+            // * The following are the default parameters for the simulation.
+            // const forceLink = d3.forceLink<ForceNodeDatum, ForceEdgeDatum>(edges).id(d => d.id)
+            // return d3
+            //   .forceSimulation(nodes)
+            //   .force("edge", forceLink.distance(100))
+            //   .force("charge", d3.forceManyBody())
+            //   .force("collide", d3.forceCollide(50).strength(0.2))
+            //   .force("center", d3.forceCenter().strength(0.05))
+            //   .alphaMin(0.001)
+        }
+      }),
+    },
   node: {
     normal: {
       type: "circle",
@@ -46,7 +79,7 @@ if (data) {
     if (node.id) {
       nodes[node.id] = { name: node.name }
       if (node.x !== undefined && node.y !== undefined) {
-        layouts.nodes[node.id] = {x: node.x, y: node.y}
+        // layouts.nodes[node.id] = {x: node.x, y: node.y}
       }
     }
   })
