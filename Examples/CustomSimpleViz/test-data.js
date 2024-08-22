f4c6dfb16a5ffe2415d1117ee970045ec691f85f
Stuart Cowan <scowan@caci.co.uk>
2024-08-22 11:10:53 +0100

    Pre-made custom viz added to examples

---
+++ Examples/CustomSimpleViz/test-data.js
@@ -0,0 +1,8 @@
+var testData = [
+    {id:1, name:"Sophie Redd", gender:"F", class1:7, class2:5, class3:9},
+    {id:2, name:"Mary May", gender:"F", class1:8, class2:6, class3:9},
+    {id:3, name:"Christine Lobowski", gender:"F", class1:5, class2:3, class3:9},
+    {id:4, name:"Brendon Philips", gender:"M", class1:9, class2:7, class3:8},
+
+    {id:5, name:"Margret Marmajuke", gender:"F", class1:5, class2:4, class3:9}
+]
