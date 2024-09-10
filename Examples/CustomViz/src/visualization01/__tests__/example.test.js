b4b6ec9d95e2f033b7c90cf54e333868fb460e60
Stuart Cowan <scowan@caci.co.uk>
2024-09-10 12:08:07 +0100

    Formatted Code + Removed Tabulator Persistence

--- Examples/CustomViz/src/visualization01/__tests__/example.test.js
+++ Examples/CustomViz/src/visualization01/__tests__/example.test.js
@@ -4,12 +4,11 @@
 
 // Put your JEST tests in here.
 
-describe('Simple test', () => {  
-    it('works', () => {
-      expect(0).toBe(0)
-    })
-    // it('fails', () => {
-    //   expect(1).toBe(0)
-    // })
-  })
-  
+describe("Simple test", () => {
+  it("works", () => {
+    expect(0).toBe(0);
+  });
+  // it('fails', () => {
+  //   expect(1).toBe(0);
+  // });
+});
