c29b90e31ac4e21b9a02fa1d4d60410c99a3eb15
Stuart Cowan <scowan@caci.co.uk>
2024-12-09 12:46:03 +0000

    New Custom Viz for React

---
+++ Examples/CustomVizReact/src/visualization/testing/test-data.js
@@ -0,0 +1,114 @@
+export const config = {"data" : {
+    "rows" : [
+                {
+                    "columns" : [
+                        {"title":"ID", "content":"1", "format":"rownum", "editable":false, "headerFilter":false, "columnSorter":false, "width":40, "frozen":true, "resizable":false},
+                        {"title":"name", "content":"Bristol", "format":null, "editable":true, "headerFilter":true, "columnSorter":true, "width":null, "frozen":false, "resizable":true}
+                    ],
+                    "groups" : [
+                        {"title":"New Group 1", "columns": [
+                            {"title":"acronym", "content":"DB", "format":null, "editable":true, "headerFilter":false, "columnSorter":true, "width":null, "frozen":false, "resizable":true}
+                        ], "subGroups": [
+                            {"title":"New Group 2", "columns": [
+                                {"title":"rating", "content":"4", "format":"star", "editable":true, "headerFilter":false, "columnSorter":true, "width":null, "frozen":false, "resizable":true},
+                                {"title":"date", "content":"2024-07-22T12:07:32Z", "format":"%dd-%MM-%yyyy", "editable":true, "headerFilter":false, "columnSorter":true, "width":null, "frozen":false, "resizable":true},
+                                {"title":"date2", "content":"2024-07-22T12:07:32Z", "format":null, "editable":true, "headerFilter":false, "columnSorter":true, "width":null, "frozen":false, "resizable":true},
+                                {"title":"date3", "content":"2024-07-22T12:07:32Z", "format":"datetime", "editable":true, "headerFilter":false, "columnSorter":true, "width":null, "frozen":false, "resizable":true}
+                        ]}
+                    ]}],
+                    "groupBy":"Domestic",
+                    "groupRows": true
+                },
+                {
+                    "columns" : [
+                        {"title":"name", "content":"Manchester United", "format":null, "editable":true}
+                    ],
+                    "groups" : [
+                        {"title":"New Group 1", "columns": [
+                            {"title":"acronym", "content":"DB", "format":null, "editable":true}
+                        ], "subGroups": [
+                            {"title":"New Group 2", "columns": [
+                                {"title":"rating", "content":"0", "format":"star", "editable":true}
+                        ]}
+                    ]}],
+                    "groupBy":"Domestic"
+                },
+                {
+                    "columns" : [
+                        {"title":"name", "content":"Spain", "format":null, "editable":true}
+                    ],
+                    "groups" : [
+                        {"title":"New Group 1", "columns": [
+                            {"title":"acronym", "content":"DB", "format":null, "editable":true}
+                        ], "subGroups": [
+                            {"title":"New Group 2", "columns": [
+                                {"title":"rating", "content":"1", "format":"star", "editable":true}
+                        ]}
+                    ]}],
+                    "groupBy":"International"
+                },
+                {
+                    "columns" : [
+                        {"title":"name", "content":"England", "format":null, "editable":true}
+                    ],
+                    "groups" : [
+                        {"title":"New Group 1", "columns": [
+                            {"title":"acronym", "content":"DB", "format":null, "editable":true}
+                        ], "subGroups": [
+                            {"title":"New Group 2", "columns": [
+                                {"title":"rating", "content":"2", "format":"star", "editable":true}
+                        ]}
+                    ]}],
+                    "groupBy":"International"
+                },
+                {
+                    "columns" : [
+                        {"title":"name", "content":"Manchester City", "format":null, "editable":true}
+                    ],
+                    "groups" : [
+                        {"title":"New Group 1", "columns": [
+                            {"title":"acronym", "content":"DB", "format":null, "editable":true}
+                        ], "subGroups": [
+                            {"title":"New Group 2", "columns": [
+                                {"title":"rating", "content":"5", "format":"star", "editable":true}
+                        ]}
+                    ]}],
+                    "groupBy":"Domestic"
+                },
+                {
+                    "columns" : [
+                        {"title":"name", "content":"Germany", "format":null, "editable":true}
+                    ],
+                    "groups" : [
+                        {"title":"New Group 1", "columns": [
+                            {"title":"acronym", "content":"DI", "format":null, "editable":true}
+                        ], "subGroups": [
+                            {"title":"New Group 2", "columns": [
+                                {"title":"rating", "content":"4", "format":"star", "editable":true}
+                        ]}
+                    ]}],
+                    "groupBy":"International"
+                },
+                {
+                    "columns" : [
+                        {"title":"name", "content":"France", "format":null, "editable":true}
+                    ],
+                    "groups" : [
+                        {"title":"New Group 1", "columns": [
+                            {"title":"acronym", "content":"DI", "format":null, "editable":true}
+                        ], "subGroups": [
+                            {"title":"New Group 2", "columns": [
+                                {"title":"rating", "content":"3", "format":"star", "editable":true}
+                        ]}
+                    ]}],
+                    "groupBy":"International"
+                }
+            ],
+    "editable": true,
+    "navigable": false,
+    "textWrap": true,
+    "theme": "modern",
+    "columnSorting": true,
+    "headerFiltering": true
+  }
+}
---
+++ Examples/CustomVizReact/webpack.prod.js
@@ -0,0 +1,25 @@
+//process.traceDeprecation = true;
+const ZipFilesPlugin = require("zip-webpack-plugin");
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const path = require("path");
+
+module.exports = merge(common, {
+  mode: "production",
+  optimization: {
+    minimize: false,
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new ZipFilesPlugin({
+      path: "..",
+      filename:
+        process.env.npm_package_name +
+        "-" +
+        process.env.npm_package_version.replace(/\./g, "-"),
+    }),
+  ],
+});
