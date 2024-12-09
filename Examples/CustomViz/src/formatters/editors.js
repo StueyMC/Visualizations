8effc345222818e6dd17b3136c8b67f50854fd3f
Stuart Cowan <scowan@caci.co.uk>
2024-12-09 12:34:03 +0000

    Expandable Columns Icon + Column Name Checking.

--- Examples/CustomViz/src/formatters/editors.js
+++ Examples/CustomViz/src/formatters/editors.js
@@ -4,14 +4,17 @@ export const getEditorType = (format) => {
   if (!format) {
     return;
   }
+  if (format === 'star') {
+    return true;
+  }
 
-  if (format.includes("%")) {
-    return "date";
+  if (format.includes('%')) {
+    return 'date';
   }
 
   if (dateFormats.includes(format)) {
-    return "date";
+    return 'date';
   }
 
-  return true;
+  return 'input';
 };
